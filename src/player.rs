a5fa979e022d05f150ee8eeb01b30502b58bc7a6
eri <eri@inventati.org>
2024-07-19 21:34:36 +0200

    feat: move animations

--- src/player.rs
+++ src/player.rs
@@ -1,9 +1,11 @@
+use std::f32::consts::PI;
+
 use bevy::prelude::*;
 
 use crate::{
     assets::SpriteAssets,
     input::{Action, ActionState},
-    tilemap::{pos_to_tile, tile_to_pos, Tile, TileType, Tilemap},
+    tilemap::{tile_to_pos, Tile, TileType, Tilemap, TILE_SEP},
     GameState, SCALE,
 };
 
@@ -21,7 +23,9 @@ impl Plugin for PlayerPlugin {
         )
         .add_systems(
             Update,
-            update_player.run_if(in_state(GameState::Play)),
+            (update_player, move_to)
+                .chain()
+                .run_if(in_state(GameState::Play)),
         );
     }
 }
@@ -31,7 +35,24 @@ impl Plugin for PlayerPlugin {
 // ··········
 
 #[derive(Component)]
-struct Player;
+struct Player {
+    pos: UVec2,
+}
+
+enum Direction {
+    North,
+    South,
+    East,
+    West,
+}
+
+#[derive(Component)]
+struct MoveTo {
+    start: Vec2,
+    target: Vec2,
+    bump_dir: Option<Direction>,
+    timer: Timer,
+}
 
 // ·······
 // Systems
@@ -48,40 +69,94 @@ fn init(mut cmd: Commands, sprite_assets: Res<SpriteAssets>) {
             layout: sprite_assets.one_bit_atlas.clone(),
             index: 25,
         },
-        Player,
+        Player {
+            pos: UVec2::new(5, 3),
+        },
     ));
 }
 
 fn update_player(
+    mut cmd: Commands,
     input: Query<&ActionState<Action>>,
-    mut player: Query<&mut Transform, With<Player>>,
+    mut player: Query<(Entity, &mut Player)>,
     tiles: Query<&Tile>,
     tilemap: Res<Tilemap>,
 ) {
-    let Ok(mut trans) = player.get_single_mut() else { return };
+    let Ok((entity, mut player)) = player.get_single_mut() else { return };
     let Ok(input) = input.get_single() else { return };
 
-    let (mut x, mut y) = pos_to_tile(trans.translation.truncate());
+    // let (mut x, mut y) = pos_to_tile(trans.translation.truncate());
+    let (mut x, mut y) = (player.pos.x, player.pos.y);
 
-    if input.just_pressed(&Action::Move) {
-        let Some(axis) = input.clamped_axis_pair(&Action::Move) else { return };
-        if axis.x().abs() > axis.y().abs() {
-            if axis.x() > 0. {
-                x = x.saturating_add(1);
-            } else {
-                x = x.saturating_sub(1);
-            }
-        } else if axis.y() > 0. {
-            y = y.saturating_add(1);
+    if !input.just_pressed(&Action::Move) {
+        return;
+    }
+
+    let Some(axis) = input.clamped_axis_pair(&Action::Move) else { return };
+    let dir = if axis.x().abs() > axis.y().abs() {
+        if axis.x() > 0. {
+            x = x.saturating_add(1);
+            Direction::East
         } else {
-            y = y.saturating_sub(1);
+            x = x.saturating_sub(1);
+            Direction::West
         }
+    } else if axis.y() > 0. {
+        y = y.saturating_add(1);
+        Direction::North
+    } else {
+        y = y.saturating_sub(1);
+        Direction::South
     };
 
     let Some(tile) = tilemap.get_tile(x, y) else { return };
     let Ok(tile) = tiles.get(tile) else { return };
+    let is_collision = matches!(tile.tile, TileType::Collision);
 
-    if !matches!(tile.tile, TileType::Collision) {
-        trans.translation = tile_to_pos(x, y).extend(10.);
-    };
+    cmd.entity(entity).insert(MoveTo {
+        start: tile_to_pos(player.pos.x, player.pos.y),
+        target: tile_to_pos(x, y),
+        bump_dir: if is_collision { Some(dir) } else { None },
+        timer: Timer::from_seconds(0.15, TimerMode::Once),
+    });
+
+    if !is_collision {
+        player.pos.x = x;
+        player.pos.y = y;
+    }
+}
+
+fn move_to(
+    mut cmd: Commands,
+    time: Res<Time>,
+    mut movables: Query<(Entity, &mut MoveTo, &mut Transform)>,
+) {
+    for (entity, mut to, mut trans) in movables.iter_mut() {
+        let timer = to.timer.tick(time.delta());
+        if timer.just_finished() {
+            cmd.entity(entity).remove::<MoveTo>();
+        }
+        let t = timer.fraction();
+
+        let pos = if let Some(dir) = &to.bump_dir {
+            let offset = (t * PI).sin() * TILE_SEP;
+            to.start + dir_to_vec(dir, offset)
+        } else {
+            to.start.lerp(to.target, t)
+        };
+        trans.translation = pos.extend(trans.translation.z);
+    }
+}
+
+// ·······
+// Helpers
+// ·······
+
+fn dir_to_vec(dir: &Direction, val: f32) -> Vec2 {
+    match dir {
+        Direction::North => Vec2::new(0., val),
+        Direction::South => Vec2::new(0., -val),
+        Direction::East => Vec2::new(val, 0.),
+        Direction::West => Vec2::new(-val, 0.),
+    }
 }
