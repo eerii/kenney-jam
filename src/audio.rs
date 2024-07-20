0d0438063ee584661862ba0faa9a35b062fe7eec
GarciaBarreiro <garcia.barreiro@proton.me>
2024-07-20 11:46:53 +0200

    feat: started menu music

--- src/audio.rs
+++ src/audio.rs
@@ -5,7 +5,7 @@ use bevy::{
     prelude::*,
 };
 
-use crate::{assets::SoundAssets, GameState};
+use crate::{assets::SoundAssets, GameState, PlayState};
 
 // ······
 // Plugin
@@ -19,8 +19,11 @@ pub struct AudioPlugin;
 
 impl Plugin for AudioPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(OnEnter(GameState::Play), init.run_if(run_once()))
-            .add_systems(Update, detect_audio_removal.run_if(in_state(GameState::Play)));
+        #[cfg(feature = "menu")]
+        app.add_systems(OnEnter(PlayState::Menu), menu_music)
+            .add_systems(OnExit(PlayState::Menu), fade_out);
+        app.add_systems(OnEnter(PlayState::Play), init.run_if(run_once()))
+            .add_systems(Update, detect_audio_removal.run_if(in_state(PlayState::Play)));
     }
 }
 
@@ -30,6 +33,9 @@ impl Plugin for AudioPlugin {
 
 #[derive(Component)]
 struct AmbientMusic;
+
+#[derive(Component)]
+struct MainMusic;
 
 // ·······
 // Systems
@@ -60,7 +66,7 @@ fn detect_audio_removal(
     mut cmd: Commands,
     assets: Res<SoundAssets>,
     mut removals: RemovedComponents<AmbientMusic>
-    ) {
+) {
     for _ in removals.read() {
         let next = rand::random::<usize>() % (assets.ambient_music.len() - 1) + 1;
         cmd.spawn((
@@ -74,5 +80,33 @@ fn detect_audio_removal(
             },
             AmbientMusic,
         ));
+    }
+}
+
+#[cfg(feature = "menu")]
+fn menu_music(mut cmd: Commands, assets: Res<SoundAssets>) {
+    cmd.spawn((
+        AudioBundle {
+            source: assets.main_menu.clone(),
+            settings: PlaybackSettings {
+                mode: PlaybackMode::Loop,
+                volume: Volume::new(1.),
+                ..default()
+            },
+        },
+        MainMusic,
+    ));
+}
+
+fn fade_out(
+    mut cmd: Commands,
+    mut sink: Query<(&mut AudioSink, Entity), With<MainMusic>>,
+    time: Res<Time>,
+) {
+    for (audio, entity) in sink.iter_mut() {
+        audio.set_volume(audio.volume() - time.delta_seconds()/0.5);
+        if audio.volume() <= 0. {
+            cmd.entity(entity).despawn_recursive();
+        }
     }
 }
